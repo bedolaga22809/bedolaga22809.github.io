a86a408bb4c07de264cd9707cf0102563a2f54cf
Alina <anitalozova@gmail.com>
2025-09-20 15:25:06 +0300

    add loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  bool f = false;
 
-  std::cout << "$ ";
-
+  while (!f) {
+  std::cout << "$ ";  
   std::string input;
   std::getline(std::cin, input);
   std::cout << input << ": command not found" << std::endl;
+  }
 }
