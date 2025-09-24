089169477f5a21e13ac29e739e851786a73b2776
Malik Ebzeev <ebz.m.d@mail.ru>
2025-09-24 23:05:38 +0300

    [tested]

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,11 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  bool f = false;
 
-  while (!f) {
-  std::cout << "$ ";  
+  std::cout << "$ ";
+
   std::string input;
   std::getline(std::cin, input);
   std::cout << input << ": command not found" << std::endl;
-  }
+
 }
